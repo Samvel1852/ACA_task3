2ede5dd129d057f57c2945602f95d5949733685f
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-04-29 13:03:58 +0400

    feat: added conditions which checks if the value is even or odd

---
+++ js/oddEven.js
@@ -0,0 +1,11 @@
+//1. Given a number. Print "odd" if the number is odd and "even" if it's even.
+let a;
+if (a % 2 === 0) {
+  console.log("odd");
+} else {
+  if (a === 0) {
+    console.log("zero");
+  } else {
+    console.log("even");
+  }
+}
