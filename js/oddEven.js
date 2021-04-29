385db860f4387b99cc8d6a36065ccb7039c1181d
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-04-29 13:07:09 +0400

    add: Added triangleAngles.js file in js directory

--- js/oddEven.js
+++ js/oddEven.js
@@ -1,11 +1,11 @@
 //1. Given a number. Print "odd" if the number is odd and "even" if it's even.
-let a;
+let a = 20;
 if (a % 2 === 0) {
-  console.log("odd");
+  console.log("even");
 } else {
   if (a === 0) {
     console.log("zero");
   } else {
-    console.log("even");
+    console.log("odd");
   }
 }
