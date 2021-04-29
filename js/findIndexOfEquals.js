78152f2b501e8f277784c490e42bd72b75918174
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-04-29 17:12:35 +0400

    feat: added feature to find the same or the nearest element's index

---
+++ js/findIndexOfEquals.js
@@ -0,0 +1,18 @@
+// 16. Given an array of numbers and a number. Find the index of a first element which is equal to that number.
+//If there is not such a number, that find the index of the first element which is the closest to it.
+let arr = [5, 46, 17, -2, 89, 0, 26];
+let num = 36;
+let diff = Infinity;
+let index = 0;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === num) {
+    console.log(arr.indexOf(arr[i]));
+  } else {
+    if (Math.abs(Math.abs(arr[i]) - Math.abs(num)) < diff) {
+      diff = Math.abs(Math.abs(arr[i]) - Math.abs(num));
+      index = arr.indexOf(arr[i]);
+    }
+  }
+}
+console.log(index);
+// console.log(diff);
