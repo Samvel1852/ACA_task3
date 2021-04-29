df6f4e098892677db10dc197a6f1ea0a6faf94f6
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-04-29 13:10:10 +0400

    feat: added action to calculate third triangle angle

---
+++ js/triangleAngles.js
@@ -0,0 +1,6 @@
+//2.Given two variables, which are the angles of a triangle. Find the third angle of the triangle. (Sum of the
+//angles of a triangle equals 180 degrees).
+let firstAngle = 75;
+let secAngle = 25;
+let thirdAngle = 180 - (firstAngle + secAngle);
+console.log(thirdAngle);
